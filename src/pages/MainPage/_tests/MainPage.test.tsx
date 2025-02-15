16720438cda5b6595b1456235c42defe4a1058a0
nsapaev <nsapaev5@gmail.com>
2025-02-15 19:33:09 +0500

    feat: add mainPage  testing

--- src/pages/MainPage/_tests/MainPage.test.tsx
+++ src/pages/MainPage/_tests/MainPage.test.tsx
@@ -1,8 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
-import { describe, vi, it, expect } from 'vitest';
-import { Routes, Route, MemoryRouter } from 'react-router-dom';
-import { CallError } from '../../../components/CallError';
+import { describe, it, expect } from 'vitest';
+import { MemoryRouter } from 'react-router-dom';
 import MainPage from '..';
 
 describe('Main Page Component', () => {
