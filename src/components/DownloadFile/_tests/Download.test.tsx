e08d8390cb38a3c18e6345042342db06cecfd881
nsapaev <nsapaev5@gmail.com>
2025-02-24 21:36:22 +0500

    fix: fix tests

--- src/components/DownloadFile/_tests/Download.test.tsx
+++ src/components/DownloadFile/_tests/Download.test.tsx
@@ -1,5 +1,5 @@
-import { describe, it, expect } from 'vitest';
-import { render, screen } from '@testing-library/react';
+import { describe, it, expect, vi, beforeAll, afterAll } from 'vitest';
+import { render, screen, fireEvent } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import { Provider } from 'react-redux';
 import { configureStore } from '@reduxjs/toolkit';
@@ -14,7 +14,28 @@ const store = configureStore({
     getDefaultMiddleware().concat(peopleApiSlice.middleware),
 });
 
+vi.mock('../../../app/hooks', () => ({
+  useAppSelector: () => [
+    {
+      name: 'Luke Skywalker',
+      height: '172',
+      hair_color: 'blond',
+      skin_color: 'fair',
+      eye_color: 'blue',
+      birth_year: '19BBY',
+      gender: 'male',
+    },
+  ],
+}));
+
 describe('Download File', () => {
+  beforeAll(() => {
+    globalThis.URL.createObjectURL = vi.fn(() => 'mock-url');
+    globalThis.URL.revokeObjectURL = vi.fn();
+  });
+  afterAll(() => {
+    vi.restoreAllMocks();
+  });
   it('should display download Button', async () => {
     render(
       <Provider store={store}>
@@ -25,5 +46,14 @@ describe('Download File', () => {
     expect(
       await screen.findByRole('button', { name: /Download/i })
     ).toBeInTheDocument();
+  });
+
+  it('calls downloadCSV function when button is clicked', () => {
+    render(<DownloadFile fileName="test.csv" />);
+
+    const button = screen.getByText('Download');
+    fireEvent.click(button);
+
+    expect(globalThis.URL.createObjectURL).toHaveBeenCalled();
   });
 });
